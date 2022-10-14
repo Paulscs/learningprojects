4f39a9934463a792bb8a3f88756afe44d223d570
Paulscs <paulscalderon@gmail.com>
2022-10-13 21:41:58 -0400

    added more to layout and a few changes to the script

--- myscripts.js
+++ myscripts.js
@@ -1,15 +1,20 @@
 let player = 0 ;
 let pc = 0;
+option = ""
+option2 = ""
 playerSelection = 0;
 computerSelection = 0;
+
 
 const picks = ["rock", "paper", "scissors"];
 const rockBtn = document.querySelector('.piedra');
 const paperBtn = document.querySelector('.papel');
 const scissorsBtn = document.querySelector('.tijeras');
 const startBtn = document.querySelector('.rectangle');
-const scoreplayer = document.querySelector(".scoreboard__score")
-const scorepc = document.querySelector(".scoreboard__score2")
+const scoreplayer = document.querySelector(".scoreboard__score");
+const scorepc = document.querySelector(".scoreboard__score2");
+const consola = document.querySelector('.textbox');
+
 
 //function to assign a random pick for the pc
 function pcpick(){
@@ -21,54 +26,80 @@ function pcpick(){
 
 //player selects 
 function playerpick(){
-    alert("Take your pick");
-    option2.addEventListener("click", )
-    const option2 = prompt("Rock, Paper, or Scissors?");
-    console.log('You chose: ' + option2);
-    return option2;
+    consola.innerHTML = "Take your pick";
+    rockBtn.addEventListener("click", rockpick);
+    paperBtn.addEventListener("click", paperpick)
+    scissorsBtn.addEventListener("click", scissorspick);
 }
+
+//rock
+function rockpick(){
+    option2 = "rock";
+    consola.innerHTML = "You chose: " + option2;
+    return option2
+}
+
+//paper
+function paperpick(){
+    option2 = "paper";
+    consola.innerHTML = "You chose: " + option2;
+    return option2
+}
+
+//scissors
+function scissorspick(){
+    option2 = "scissors";
+    consola.innerHTML = "You chose: " + option2;
+    return option2
+}
+
 
 //game function for the 5 rounds
 function game(){
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 5; i++) {
         playRound();
-        console.log("You have " + player +" points")
-        console.log("The opponent has " + pc +" points" )
         if (player == 3) {
-            console.log("You win!")
+            consola.innerHTML = "You win!";
             break;
         }
         else if (pc == 3) {
-            console.log("You lost!")
+            consola.innerHTML = "You lose!";
             break;
         }
     }
 }
+
 
 function playRound() {
     let playerSelection = playerpick();
     let computerSelection = pcpick();
+    console.log(playerSelection);
+
     if (playerSelection == computerSelection) {
         console.log('TIE GAME! Round restarts');
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        console.log('player wins!');
+        consola.innerHTML = 'player wins!';
         player++
-        console.log("Next round stards...")
+        scoreplayer.innerHTML = player;
+        consola.innerHTML += "Next round starts...";
     } 
     else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        console.log('player wins!');
-        console.log("Next round stards...")
+        consola.innerHTML = 'player wins!';
         player++
+        scoreplayer.innerHTML = player;
+        consola.innerHTML += "Next round starts...";
     } 
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('player wins!');
-        console.log("Next round stards...")
+        consola.innerHTML = 'player wins!';
         player++
+        scoreplayer.innerHTML = player;
+        consola.innerHTML += " Next round starts...";
     } 
     else {
-        console.log('You Lose!');
+        consola.innerHTML = 'You lose!';
         pc++
+        scorepc.innerHTML = pc;
     }
 }
 
